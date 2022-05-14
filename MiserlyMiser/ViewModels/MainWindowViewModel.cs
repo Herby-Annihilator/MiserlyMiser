ed4caec8b64540518909c5e2939cc233b80bae0e
Rukin Danil <rukin2018d@yandex.ru>
2022-05-14 15:49:30 +0700

    start

---
+++ MiserlyMiser/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MiserlyMiser.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Markup;
+
+namespace MiserlyMiser.ViewModels
+{
+    [MarkupExtensionReturnType(typeof(MainWindowViewModel))]
+    public class MainWindowViewModel : ViewModel
+    {
+        #region Properties
+        private string _title = "Title";
+        public string Title { get => _title; set => Set(ref _title, value); }
+
+        private string _status = "Status";
+        public string Status { get => _status; set => Set(ref _status, value); }
+        #endregion
+
+        #region Commands
+
+        #endregion
+    }
+}
---
+++ MiserlyMiser/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiserlyMiser.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public MainWindowViewModel MainWindowModel => App.Services.GetRequiredService<MainWindowViewModel>();
+    }
+}
